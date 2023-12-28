2a7bbad99c82283f4c651a29d532082fa4395e0c
Krysmal <malinowskizxcvbnm@gmail.com>
2023-12-28 23:12:20 +0100

    start

--- AutoBackup/Form1.cs
+++ AutoBackup/Form1.cs
@@ -2,9 +2,23 @@ namespace AutoBackup
 {
     public partial class Form1 : Form
     {
+        string path;
+        string[] files;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            path=textBox1.Text;
+            System.Windows.Forms.MessageBox.Show(path);
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
