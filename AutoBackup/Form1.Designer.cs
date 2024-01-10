e41ff02fe4f824b10c1f656ae2d59be17e68cee0
Krysmal <malinowskizxcvbnm@gmail.com>
2024-01-10 21:17:45 +0100

    work_in_progres

--- AutoBackup/Form1.Designer.cs
+++ AutoBackup/Form1.Designer.cs
@@ -32,11 +32,13 @@
             this.button2 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(150, 67);
+            this.button1.Location = new System.Drawing.Point(157, 78);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(94, 29);
             this.button1.TabIndex = 0;
@@ -46,7 +48,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(568, 67);
+            this.button2.Location = new System.Drawing.Point(536, 78);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(94, 29);
             this.button2.TabIndex = 1;
@@ -56,10 +58,12 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(36, 34);
+            this.textBox1.Location = new System.Drawing.Point(36, 45);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(729, 27);
             this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "z";
+            this.textBox1.Click += new System.EventHandler(this.textBox1_Click);
             // 
             // listBox1
             // 
@@ -67,14 +71,34 @@
             this.listBox1.ItemHeight = 20;
             this.listBox1.Location = new System.Drawing.Point(36, 108);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(729, 284);
+            this.listBox1.Size = new System.Drawing.Size(342, 284);
             this.listBox1.TabIndex = 3;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 20;
+            this.listBox2.Location = new System.Drawing.Point(401, 108);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(364, 284);
+            this.listBox2.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(36, 12);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(729, 27);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.Text = "do";
+            this.textBox2.Click += new System.EventHandler(this.textBox2_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.listBox2);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button2);
@@ -92,5 +116,7 @@
         private Button button2;
         private TextBox textBox1;
         private ListBox listBox1;
+        private ListBox listBox2;
+        private TextBox textBox2;
     }
 }
--- AutoBackup/Form1.cs
+++ AutoBackup/Form1.cs
@@ -1,27 +1,31 @@
 using System.Windows.Forms;
 using System.Xml.Linq;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace AutoBackup
 {
     public partial class Form1 : Form
     {
-        string path;
+        string pathFrom,pathTo;
         string[] files;
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        
+        
+
+           private void button1_Click(object sender, EventArgs e)
         {
-            path=textBox1.Text;
-            
-            if (Directory.Exists(path))
+            listBox1.Items.Clear();
+            listBox2.Items.Clear();
+            pathFrom=textBox1.Text;
+            pathTo = textBox2.Text;
+            if (Directory.Exists(pathFrom))
             {
-                string[] foldery = Directory.GetDirectories(path);
-                string[] pliki = Directory.GetFiles(path);
-
-
+                string[] foldery = Directory.GetDirectories(pathFrom);
+                string[] pliki = Directory.GetFiles(pathFrom);
 
                 listBox1.Items.Add("Foldery: ");
 
@@ -42,7 +46,52 @@ namespace AutoBackup
             }
             else
             {
-                System.Windows.Forms.MessageBox.Show("b³êdna œcie¿ka");
+                System.Windows.Forms.MessageBox.Show("b³êdna œcie¿ka wejœciowa");
+            }
+
+            
+
+            if (Directory.Exists(pathTo))
+            {
+                string[] foldery = Directory.GetDirectories(pathTo);
+                string[] pliki = Directory.GetFiles(pathTo);
+
+                listBox2.Items.Add("Foldery: ");
+
+                foreach (string folder in foldery)
+                {
+                    listBox2.Items.Add(folder);
+                }
+                listBox2.Items.Add("");
+                listBox2.Items.Add("");
+                listBox2.Items.Add("");
+                listBox2.Items.Add("Pliki: ");
+
+                foreach (string element in pliki)
+                {
+
+                    listBox2.Items.Add(element);
+                }
+            }
+            else
+            {
+                System.Windows.Forms.MessageBox.Show("b³êdna œcie¿ka wyjœciowa");
+            }
+        }
+
+        private void textBox1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "z")
+            {
+                textBox1.Text = string.Empty;
+            }
+        }
+
+        private void textBox2_Click(object sender, EventArgs e)
+        {
+            if (textBox2.Text == "do")
+            {
+                textBox2.Text = string.Empty;
             }
         }
 
